9b056f83df4f8cbe694af25169aa8d2d3eb5b6dd
serg0761 <36167730+serg0761@users.noreply.github.com>
2019-07-08 08:49:42 +0500

    Create hh2.py

---
+++ hh2.py
@@ -0,0 +1,90 @@
+from bs4 import BeautifulSoup as bs
+import requests
+from pymongo import MongoClient
+
+
+def get_link(lang, page):
+    vacancy = 'программист'
+    link = 'https://ekaterinburg.hh.ru/search/vacancy?text=' + vacancy + '+' + lang + \
+    '&page=' + str(page)
+    return link
+    
+
+
+def hh_parse(link, head):
+    result =  {}
+    session = requests.Session()
+    request = session.get(link,headers = head)
+    if request.status_code == 200:
+        soup = bs(request.content, 'html.parser')
+        divs = soup.find_all('div', attrs={'data-qa':'vacancy-serp__vacancy'})
+        for div in divs:
+            title = div.find('a', attrs={'data-qa':'vacancy-serp__vacancy-title'}).text
+            href = div.find('a', attrs={'data-qa':'vacancy-serp__vacancy-title'})['href']
+            salary = div.find('div', attrs={'data-qa':'vacancy-serp__vacancy-compensation'})
+            if salary is None:
+                min_salary = '--'
+                max_salary = '--'
+            else:
+                currency = salary.text[-4:].strip().lower()
+                if currency == 'eur':
+                    k = 71.66
+                elif currency == 'usd':
+                    k = 63.58
+                else:
+                    k = 1
+                first_word = salary.text[:2].lower()
+                spam = salary.text.split('-')
+                spam_salary = []
+                for sal in spam:
+                    spam_s = sal.split()
+                    for spams in spam_s:
+                        try:
+                            spam_salary.append(int(spams))
+                        except ValueError:
+                            pass
+                res_salary = []
+                for i in range(0, len(spam_salary), 2):
+                    if spam_salary[i + 1] == 0:
+                        res_salary.append(int(str(spam_salary[i]) + '000') * k)
+                    else:
+                        res_salary.append(int(str(spam_salary[i]) + str(spam_salary[i + 1])) * k)
+                if first_word == 'от':
+                    min_salary = min(res_salary)
+                    max_salary = '--'
+                elif first_word == 'до':
+                    max_salary = max(res_salary)
+                    min_salary = '--'
+                else:
+                    min_salary = min(res_salary)
+                    max_salary = max(res_salary)
+                    
+                
+            result[str(title)] = href, min_salary, max_salary
+    else:
+        print('NO OK')
+    
+    return result
+ 
+
+def to_db(data):
+    hhdbs.insert(data)
+
+
+
+
+
+        
+lang = str(input('Введите язык программирования?'))
+
+head = {'accept':'*/*', 
+    'user-agent':'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
+
+
+vacancies = {}
+for page in range(0, 2):
+    link = get_link(lang, page)
+    vacancies.update()
+
+
+print(vacancies)
