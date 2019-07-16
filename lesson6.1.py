dd41aa23e56fbbea4f8c6a64ce0ad957fa98d654
serg0761 <36167730+serg0761@users.noreply.github.com>
2019-07-16 06:52:31 +0500

    Add files via upload

---
+++ lesson6.1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 06:50:51 2019
+
+@author: snboyarskiy
+"""
+
---
+++ lesson6.2.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 16 06:51:23 2019
+
+@author: snboyarskiy
+"""
+
